63ff342bdf4f77aa4a3d02b96902c639a118c652
rajesh <merajesh04@gmail.com>
2021-04-18 13:22:35 +0530

    * create a scene * add objects * group objects for rotation and revolutions

---
+++ sketch.js
@@ -0,0 +1,90 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require('three');
+
+// Include any additional ThreeJS examples below
+require('three/examples/js/controls/OrbitControls');
+
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: 'webgl',
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas,
+  });
+
+  // WebGL background color
+  renderer.setClearColor('#000', 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+  const loader = new THREE.TextureLoader();
+  // load the earth map texture
+  const earthTexture = loader.load('earth.jpg');
+  const moonTexture = loader.load('moon.jpg');
+
+  // Setup a material
+  const earthMaterial = new THREE.MeshBasicMaterial({
+    map: earthTexture,
+  });
+
+  // Setup a mesh with geometry + material
+  const earthMesh = new THREE.Mesh(geometry, earthMaterial);
+  scene.add(earthMesh);
+
+  const moonGroup = new THREE.Group();
+  const moonMaterial = new THREE.MeshBasicMaterial({
+    map: moonTexture,
+  });
+
+  const moonMesh = new THREE.Mesh(geometry, moonMaterial);
+  moonMesh.position.set(1, 0, 2);
+  moonMesh.scale.setScalar(0.25);
+  moonGroup.add(moonMesh);
+
+  scene.add(moonGroup);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      earthMesh.rotation.y = time * 0.2;
+      moonMesh.rotation.y = time * 0.14;
+      moonGroup.rotation.y = time * 0.4;
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
